728d1440f98b76a293ee730c1447d9742677f581
Queenie-7 <522356165@qq.com>
2020-07-10 23:06:57 +0800

    Update [10]II青蛙跳台阶问题.java

--- 剑指offer/[10]II青蛙跳台阶问题.java
+++ 剑指offer/[10]II青蛙跳台阶问题.java
@@ -24,7 +24,7 @@ class numWays{
         dp[0]=1;
         dp[1]=1;
         for(int i=2;i<n+1;i++){
-            dp[i]= dp[i-1] + dp[i-2];
+            dp[i]= (dp[i-1] + dp[i-2])% 1000_000_007;
         }
         return dp[n];
     }
